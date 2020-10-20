308929fdfb5565fd0b851313acffa9642276b80d
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2020-10-20 10:08:46 -0300

    fix check issues with argo_prof()

--- R/argo-prof.R
+++ R/argo-prof.R
@@ -1,7 +1,8 @@
 
 #' Load Argo profiles
 #'
-#' @inheritParams argo_url
+#' @inheritParams argo_download
+#' @inheritParams argo_nc_prof_read
 #' @param file A previously downloaded Argo profile NetCDF file.
 #'
 #' @return A [tibble::tibble()].
@@ -12,12 +13,14 @@ argo_prof <- function(path, vars = NULL, download = NULL, quiet = FALSE) {
   assert_argo_prof_file(path)
   cached <- argo_download(path, download = download, quiet = quiet)
 
-  tbls <- lapply(cached, argo_read_prof, meta = c("CYCLE_NUMBER", "JULD", "LATITUDE", "LONGITUDE"))
-  if (is.null(vars)) {
-    tbl <- vctrs::vec_rbind(!!! tbls)
-  } else {
-    tbl <- do.call(rbind, quote(tbls))
-  }
+  tbls <- lapply(
+    cached,
+    argo_read_prof,
+    vars = if (!is.null(vars)) toupper(vars) else vars,
+    meta = c("CYCLE_NUMBER", "JULD")
+  )
+
+  tbl <- vctrs::vec_rbind(!!! tbls)
 
   # make names lowercase
   names(tbl) <- tolower(names(tbl))
--- tests/testthat/test-argo-prof.R
+++ tests/testthat/test-argo-prof.R
@@ -1,24 +1,36 @@
 
 test_that("argo_prof() works", {
-  prof <- with_argo_mirror(
-    argo_test_mirror(),
-    argo_prof("dac/csio/2900313/profiles/D2900313_000.nc", quiet = TRUE)
-  )
+  with_argo_mirror(argo_test_mirror(), {
+    prof <- argo_prof("dac/csio/2900313/profiles/D2900313_000.nc", quiet = TRUE)
+    expect_true(all(c("cycle_number", "pres", "temp") %in% names(prof)))
+    expect_true(all(prof$cycle_number == 0))
 
-  expect_true(FALSE)
+    prof <- argo_prof(
+      "dac/csio/2900313/profiles/D2900313_000.nc",
+      vars = c("pres", "temp", "empty"),
+      quiet = TRUE
+    )
+
+    expect_identical(names(prof), c("float", "cycle_number", "date", "pres", "temp", "empty"))
+  })
 })
 
 test_that("argo_read_prof() works", {
   expect_is(
-    argo_read_prof("inst/cache-test/dac/csio/2900313/profiles/D2900313_000.nc"),
+    argo_read_prof(
+      system.file(
+        "cache-test/dac/csio/2900313/profiles/D2900313_000.nc",
+        package = "argodata"
+      )
+    ),
     "tbl_df"
   )
 })
 
 test_that("assert_argo_prof_file() works", {
   expect_identical(
-    assert_argo_prof_file("aoml/3901309/profiles/R3901309_001D.nc"),
-    "aoml/3901309/profiles/R3901309_001D.nc"
+    assert_argo_prof_file("dac/aoml/3901309/profiles/R3901309_001D.nc"),
+    "dac/aoml/3901309/profiles/R3901309_001D.nc"
   )
   expect_error(assert_argo_prof_file("not a file"), "Found 1 invalid Argo profile path")
 })

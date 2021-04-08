c0b0dfcad300d05c063c93614d368f6a5e9d7bcc
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-04-08 16:16:08 -0300

    remove argo-nc-traj, move tests to test-argo-traj

--- tests/testthat/test-argo-traj.R
+++ tests/testthat/test-argo-traj.R
@@ -2,7 +2,12 @@
 test_that("argo_traj_measurement() works", {
   with_argo_example_cache({
     traj <- argo_traj_measurement("dac/csio/2900313/2900313_Rtraj.nc", quiet = TRUE)
-    expect_true(all(c("file", "cycle_number", "longitude", "latitude") %in% names(traj)))
+    expect_true(
+      all(
+        c("file", "cycle_number", "longitude", "latitude") %in%
+          names(traj)
+      )
+    )
 
     traj <- argo_traj_measurement(
       "dac/csio/2900313/2900313_Rtraj.nc",
@@ -10,7 +15,10 @@ test_that("argo_traj_measurement() works", {
       quiet = TRUE
     )
 
-    expect_identical(names(traj), c("file", "n_measurement", "cycle_number", "longitude"))
+    expect_identical(
+      names(traj),
+      c("file", "n_measurement", "cycle_number", "longitude")
+    )
   })
 })
 
@@ -32,7 +40,12 @@ test_that("argo_traj_cycle() works", {
 test_that("argo_traj_history() works", {
   with_argo_example_cache({
     traj <- argo_traj_history("dac/csio/2900313/2900313_Rtraj.nc", quiet = TRUE)
-    expect_true(all(c("file", "history_index_dimension", "history_software") %in% names(traj)))
+    expect_true(
+      all(
+        c("file", "history_index_dimension", "history_software") %in%
+          names(traj)
+      )
+    )
 
     traj <- argo_traj_history(
       "dac/csio/2900313/2900313_Rtraj.nc",
@@ -126,4 +139,59 @@ test_that("assert_argo_traj_file() works", {
   })
 
   expect_error(assert_argo_traj_file("not a file"), "Found 1 invalid Argo trajectory path")
+})
+
+test_that("argo_read_traj_measurement() works for trajectory files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_Rtraj.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_traj_measurement(nc)
+  expect_true(all(c("N_MEASUREMENT", "JULD", "JULD_QC") %in% names(nc_all)))
+
+  nc_no_meta <- argo_read_traj_measurement(
+    nc,
+    vars = c("LONGITUDE", "LATITUDE", "EMPTY"),
+    quiet = TRUE
+  )
+  expect_identical(names(nc_no_meta), c("N_MEASUREMENT", "LONGITUDE", "LATITUDE"))
+})
+
+test_that("argo_read_traj_cycle() works for trajectory files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_Rtraj.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_traj_cycle(nc)
+  expect_true(
+    all(
+      c("N_CYCLE", "JULD_DESCENT_START", "JULD_DESCENT_START_STATUS") %in% names(nc_all)
+    )
+  )
+
+  nc_no_meta <- argo_read_traj_cycle(nc, vars = c("CYCLE_NUMBER_INDEX", "EMPTY"), quiet = TRUE)
+  expect_identical(names(nc_no_meta), c("N_CYCLE", "CYCLE_NUMBER_INDEX"))
+})
+
+test_that("argo_read_traj_history() works for trajectory files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_Rtraj.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_traj_history(nc)
+  expect_true(
+    all(
+      c("N_HISTORY", "HISTORY_PREVIOUS_VALUE", "HISTORY_STEP") %in% names(nc_all)
+    )
+  )
+
+  nc_no_meta <- argo_read_traj_history(
+    nc,
+    vars = c("HISTORY_INDEX_DIMENSION", "HISTORY_QCTEST", "EMPTY"),
+    quiet = TRUE
+  )
+  expect_identical(names(nc_no_meta), c("N_HISTORY", "HISTORY_INDEX_DIMENSION", "HISTORY_QCTEST"))
 })

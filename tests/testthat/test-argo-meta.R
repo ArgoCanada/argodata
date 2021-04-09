dc201fa6a0a3ad11c13fd156f3dd4b6ebf56bbfe
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-04-09 11:09:42 -0300

    fix tests, remove argo-nc-meta

--- tests/testthat/test-argo-meta.R
+++ tests/testthat/test-argo-meta.R
@@ -172,4 +172,116 @@ test_that("argo_read_meta_param() works", {
 })
 
 
+test_that("argo_read_meta_config_param() works for meta files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_meta.nc",
+    package = "argodata"
+  )
 
+  nc_all <- argo_read_meta_config_param(nc)
+  expect_true(
+    all(
+      c("CONFIG_PARAMETER_NAME",  "CONFIG_PARAMETER_VALUE")
+      %in% names(nc_all)
+    )
+  )
+})
+
+test_that("argo_read_meta_missions() works for meta files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_meta.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_meta_missions(nc)
+  expect_true(
+    all(
+      c("CONFIG_MISSION_NUMBER", "CONFIG_MISSION_COMMENT")
+      %in% names(nc_all)
+    )
+  )
+})
+
+test_that("argo_read_meta_trans_system() works for meta files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_meta.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_meta_trans_system(nc)
+  expect_true(
+    all(
+      c("TRANS_SYSTEM", "TRANS_SYSTEM_ID", "TRANS_FREQUENCY")
+      %in% names(nc_all)
+    )
+  )
+})
+
+
+test_that("argo_read_meta_positioning_system() works for meta files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_meta.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_meta_positioning_system(nc)
+  expect_true(
+    all(
+      c("POSITIONING_SYSTEM")
+      %in% names(nc_all)
+    )
+  )
+})
+
+test_that("argo_read_meta_launch_config_param() works for meta files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_meta.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_meta_launch_config_param(nc)
+  expect_true(
+    all(
+      c("LAUNCH_CONFIG_PARAMETER_NAME", "LAUNCH_CONFIG_PARAMETER_VALUE")
+      %in% names(nc_all)
+    )
+  )
+})
+
+test_that("argo_read_meta_sensor() works for meta files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_meta.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_meta_sensor(nc)
+  expect_true(
+    all(
+      c("SENSOR", "SENSOR_MAKER", "SENSOR_MODEL", "SENSOR_SERIAL_NO")
+      %in% names(nc_all)
+    )
+  )
+})
+
+test_that("argo_read_meta_param() works for meta files", {
+  nc <- system.file(
+    "cache-test/dac/csio/2900313/2900313_meta.nc",
+    package = "argodata"
+  )
+
+  nc_all <- argo_read_meta_param(nc)
+  expect_true(
+    all(
+      c(
+        "PARAMETER",
+        "PARAMETER_SENSOR",
+        "PARAMETER_UNITS",
+        "PARAMETER_ACCURACY",
+        "PARAMETER_RESOLUTION",
+        "PREDEPLOYMENT_CALIB_EQUATION",
+        "PREDEPLOYMENT_CALIB_COEFFICIENT",
+        "PREDEPLOYMENT_CALIB_COMMENT"
+      ) %in% names(nc_all)
+    )
+  )
+})
